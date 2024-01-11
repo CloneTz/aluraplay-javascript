db5550f5037d5a5e744f2c390e1dface48a8a344
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2024-01-11 14:13:26 -0300

    agora é possivel adicionar videos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,8 +5,28 @@ async function listaVideos() {
   return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+  const conexao = await fetch("http://localhost:3000/videos", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      url: url,
+      imagem: imagem,
+    }),
+  });
+
+  const conexaoConvertida = await conexao.JSON();
+
+  return conexaoConvertida;
+}
+
 const conectaApi = {
   listaVideos,
+  criaVideo,
 };
 
 export { conectaApi };
